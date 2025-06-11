004197b08f66d16860cb096b6b33651df6830ed7
Sahaphab Makchalerm <web24678@gmail.com>
2025-06-11 16:32:35 +0700

    Start upgrade todo for Edit

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Todo } from "../features/todo/types";
 import { useTodoStore } from "../store/todoStore";
 
@@ -8,30 +9,56 @@ type Props = {
 export const TodoItem = ({ todo }: Props) => {
   const toggleTodo = useTodoStore((state) => state.toggleTodo);
   const deleteTodo = useTodoStore((state) => state.deleteTodo);
+  const updateTodo = useTodoStore((state) => state.updateTodo);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editText, setEditText] = useState(todo.text);
+
+  const handleUpdate = () => {
+    if (editText.trim() === "") return;
+    updateTodo(todo.id, editText);
+    setIsEditing(false);
+  };
 
   return (
-    <div className="flex items-center justify-between p-2 border rounded-lg shadow-sm bg-white">
-      <span
-        className={`cursor-pointer flex-1 ${
-          todo.completed ? "line-through text-gray-400" : ""
-        }`}
-        onClick={() => toggleTodo(todo.id)}
-      >
-        {todo.text}
-      </span>
-      <p className="text-xs text-gray-500 mt-1">
-        Created: {new Date(todo.createdAt).toLocaleString()}
-      </p>
-      <p className="text-xs text-gray-500">
-        Updated: {new Date(todo.updatedAt).toLocaleString()}
-      </p>
+    <div className="flex flex-col bg-white p-5 rounded shadow">
+      <div className="flex items-center justify-between">
+        <div className="w-[90%] break-words">
+          {isEditing ? (
+            <input
+              className="w-full break-words"
+              value={editText}
+              onChange={(e) => setEditText(e.target.value)}
+              onBlur={handleUpdate}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") handleUpdate();
+              }}
+              autoFocus
+            />
+          ) : (
+            <span
+              className={`cursor-pointer`}
+              onClick={() => setIsEditing(true)}
+            >
+              {todo.text}
+            </span>
+          )}
+        </div>
+        <div className="w-[10%] flex justify-end">
+          <button
+            onClick={() => deleteTodo(todo.id)}
+            title="Delete"
+            className="text-red-500 hover:text-red-700 ml-2"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
 
-      <button
-        onClick={() => deleteTodo(todo.id)}
-        className="ml-4 text-red-500 hover:text-red-700"
-      >
-        Delete
-      </button>
+      <div className="flex justify-between text-xs text-gray-500 mt-1">
+        <p>Created: {new Date(todo.createdAt).toLocaleString()}</p>
+        <p>Updated: {new Date(todo.updatedAt).toLocaleString()}</p>
+      </div>
     </div>
   );
 };
